568d933309ea09748c3d96967afa6f0c635f64a1
Niels Wiesner <niels.wiesner@web.de>
2018-08-29 20:36:10 +0200

    new class

--- Vieleck/Program.cs
+++ Vieleck/Program.cs
@@ -16,7 +16,7 @@ namespace Vieleck
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_start());
         }
     }
 }
